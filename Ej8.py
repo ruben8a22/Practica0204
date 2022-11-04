71278d93a7195aa4a0cf7b439ed61d411ecadca3
ruben8a22 <114002321+ruben8a22@users.noreply.github.com>
2022-11-04 18:18:20 +0100

    Update Ej8.py

--- Ej8.py
+++ Ej8.py
@@ -1,6 +1,6 @@
 #Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
 
-for num1 in range(1,11):
-    for num2 in range(1,11):
-        resultado = num1 * num2
-        print(num1,'x', num2, '=', resultado)
+for numero1 in range(1, 11):
+    for numero2 in range(1, 11):
+        resultado = numero1 * numero2
+        print(numero1, 'x', numero2, '=', resultado)
